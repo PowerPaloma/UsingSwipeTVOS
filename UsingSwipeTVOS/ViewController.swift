e7c546f2d627221f0cec60958d3f08f39195a05e
Paloma Bispo <palomabispo@alu.ufc.br>
2019-04-05 08:28:43 -0300

    Initial Commit

---
+++ UsingSwipeTVOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsingSwipeTVOS
+//
+//  Created by Paloma Bispo on 05/04/19.
+//  Copyright © 2019 Paloma Bispo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
